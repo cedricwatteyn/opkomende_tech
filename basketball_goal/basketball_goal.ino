3b8e37d8f15dcc4a046d96c44eb209811ec83402
cedricwatteyn <154930366+cedricwatteyn@users.noreply.github.com>
2025-09-04 14:05:19 +0200

    Update basketball_goal.ino

--- basketball_goal/basketball_goal.ino
+++ basketball_goal/basketball_goal.ino
@@ -1,4 +1,6 @@
 #include <Adafruit_NeoPixel.h>
+#include <SoftwareSerial.h>
+#include <DFRobotDFPlayerMini.h>
 
 const int irReceiverPin = 2;    // Grove IR receiver
 const int ky032VCC = 4;         // KY-032 IR zender VCC
@@ -16,15 +18,13 @@ SoftwareSerial mySerial(10, 11); // RX, TX
 DFRobotDFPlayerMini myDFPlayer;
 
 // ---------- Game state ----------
->>>>>>> 22b6c9342c6c4a2b64494c2aec90478451b7b878
 bool countdownActive = false;
 unsigned long countdownStartTime = 0;
 bool beamBroken = false;
 bool piezzoHit = false;
 
-
 int currentPixel = 0;
-int score = 0;            // Score teller
+int score = 0;             // Score teller
 int scoreWithoutBoard = 0; // Score zonder bord
 
 // ---------- Piezzo drempel ----------
@@ -32,20 +32,10 @@ const int piezzoThreshold = 100; // waarde afhankelijk van piezzo, moet getest w
 
 void setup() {
   // ---------- Input/Output ----------
->>>>>>> 22b6c9342c6c4a2b64494c2aec90478451b7b878
   pinMode(irReceiverPin, INPUT);
   pinMode(ky032VCC, OUTPUT);
   pinMode(platePin, INPUT_PULLUP);
   pinMode(ledPin, OUTPUT);
-<<<<<<< HEAD
-  pinMode(platePin, INPUT_PULLUP);
-  Serial.begin(9600);
-
-  // Initialize Neopixel
-  pixels.begin();
-  pixels.clear();
-  pixels.show();
-=======
 
   // Zet KY-032 zender aan
   digitalWrite(ky032VCC, HIGH);
@@ -65,24 +55,16 @@ void setup() {
   }
   Serial.println("DFPlayer Mini online.");
   myDFPlayer.volume(20); // volume 0-30
->>>>>>> 22b6c9342c6c4a2b64494c2aec90478451b7b878
 }
 
 void loop() {
   // ---------- Lees inputs ----------
   int beamState = digitalRead(irReceiverPin);
-<<<<<<< HEAD
-  int plateState = digitalRead(platePin); // LOW wanneer ingedrukt
-
-  // Start tellen wanneer pressure plate ingedrukt is
-  if (!countdownActive && plateState == LOW) {
-=======
   int startState = digitalRead(platePin);
   int piezzoValue = analogRead(piezzoPin);
 
   // ---------- Start countdown ----------
   if (!countdownActive && startState == LOW) {
->>>>>>> 22b6c9342c6c4a2b64494c2aec90478451b7b878
     countdownActive = true;
     countdownStartTime = millis();
     beamBroken = false;
@@ -91,23 +73,6 @@ void loop() {
   }
 
   if (countdownActive) {
-<<<<<<< HEAD
-    // Check of de straal onderbroken wordt
-    if (beamState == HIGH) { // Beam broken
-      beamBroken = true;
-    }
-
-    // Na 5 seconden, bekijk resultaat
-    if (millis() - countdownStartTime >= 5000) {
-      if (beamBroken) {
-        Serial.println("Score!");
-        digitalWrite(ledPin, HIGH);
-        pixels.setPixelColor(currentPixel, pixels.Color(0, 255, 0)); // Green
-      } else {
-        Serial.println("Miss!");
-        digitalWrite(ledPin, LOW);
-        pixels.setPixelColor(currentPixel, pixels.Color(255, 0, 0)); // Red
-=======
     // Check IR beam (Grove IR sensor)
     if (beamState == HIGH) { // HIGH betekent onderbroken
       beamBroken = true;
@@ -121,48 +86,3 @@ void loop() {
     // Na 5 seconden countdown
     if (millis() - countdownStartTime >= 5000) {
       if (beamBroken) {
-        if (piezzoHit) {
-          score++; // Score met bord
-          Serial.print("Score + bord! Totaal = ");
-          Serial.println(score);
-
-          digitalWrite(ledPin, HIGH);
-          pixels.setPixelColor(currentPixel, pixels.Color(0, 255, 0)); // Groen
-          myDFPlayer.play(1); // Score geluid
-        } else {
-          scoreWithoutBoard++; // Score zonder bord
-          Serial.print("Score zonder bord! Totaal = ");
-          Serial.println(scoreWithoutBoard);
-
-          digitalWrite(ledPin, LOW);
-          pixels.setPixelColor(currentPixel, pixels.Color(255, 0, 0)); // Rood
-          myDFPlayer.play(2); // Miss/zonder bord geluid
-        }
-      } else {
-        Serial.println("Miss!");
-        digitalWrite(ledPin, LOW);
-        pixels.setPixelColor(currentPixel, pixels.Color(0, 0, 255)); // Blauw = geen score
-        myDFPlayer.play(2); // Miss geluid
->>>>>>> 22b6c9342c6c4a2b64494c2aec90478451b7b878
-      }
-      pixels.show();
-
-<<<<<<< HEAD
-      // Move to the next pixel for the next round
-      currentPixel++;
-      if (currentPixel >= numPixels) currentPixel = 0;
-
-      countdownActive = false; // Reset for next round
-=======
-      pixels.show();
-
-      // Volgende pixel
-      currentPixel++;
-      if (currentPixel >= numPixels) currentPixel = 0;
-
-      // Reset countdown
-      countdownActive = false;
->>>>>>> 22b6c9342c6c4a2b64494c2aec90478451b7b878
-    }
-  }
-}
